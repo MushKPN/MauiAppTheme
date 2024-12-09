77db504aa49b76e39a6ad6c70f70a6b3c3cbd79d
Camille MACÉ <camille@optavis.fr>
2024-12-09 16:58:03 +0100

    Added repro for ListPicker

--- MauiTheme/ViewModel/OptionsViewModel.cs
+++ MauiTheme/ViewModel/OptionsViewModel.cs
@@ -1,12 +1,19 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace MauiTheme.ViewModel;
 
 public partial class OptionsViewModel : BaseViewModel
 {
+    [ObservableProperty]
+    private ObservableCollection<string> myItems = [];
+
     public OptionsViewModel(IServiceProvider provider) : base(provider)
     {
-
+        MyItems.Add("A");
+        MyItems.Add("B");
+        MyItems.Add("C");
     }
 
     [RelayCommand]
